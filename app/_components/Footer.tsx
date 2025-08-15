9236d440417109b114a2ed7395089f66bdfe973a
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-08-15 13:22:31 +0300

    Update Footer.tsx

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col bg-white dark:bg-gray-900">
       <footer className="flex flex-col gap-4 py-6 w-full shrink-0 items-center px-4 md:px-6">
-        <div className="flex items-baseline justify-center space-x-6">
+        <div className="flex-none md:flex text-center items-baseline justify-center space-x-6">
           For inspiration: <span className="text-slate-700 ms-1">UQCT...ESYA</span> <CopyButton text="UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA" /> <span className="ms-3">| &hearts; Toncoin</span>
         </div>
       </footer>
