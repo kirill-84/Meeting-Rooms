bfc56511171a8729398eee049ccc742511bbfaf6
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-08-15 11:37:11 +0300

    Update Footer.tsx

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,9 +4,9 @@ import { CopyButton } from "@/components/copy-button/CopyButton"
 
 export default function Footer() {
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
+    <div className="flex flex-col bg-white dark:bg-gray-900">
       <footer className="flex flex-col gap-4 py-6 w-full shrink-0 items-center px-4 md:px-6">
-        <div className="flex justify-center space-x-6">
+        <div className="flex items-baseline justify-center space-x-6">
           For inspiration: <span className="text-slate-700">UQCT...ESYA</span> <CopyButton text="UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA" /> | &hearts; Toncoin
         </div>
       </footer>
