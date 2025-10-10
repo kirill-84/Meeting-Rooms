3d88b0b501fcd21895bce58a3772749fd5c76ab2
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-10-10 09:19:07 +0300

    Update my address TON

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <div className="flex flex-col bg-white dark:bg-gray-900">
       <footer className="flex flex-col gap-4 py-6 w-full shrink-0 items-center px-4 md:px-6">
         <div className="flex-none md:flex text-center items-baseline justify-center space-x-6">
-          For inspiration: <span className="text-slate-700 ms-1">UQCT...ESYA</span> <CopyButton text="UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA" /> <span className="ms-3">| &hearts; Toncoin</span>
+          For inspiration: <span className="text-slate-700 ms-1">UQBw...o5FI</span> <CopyButton text="UQBwJX9X7no3RSaiHM9dke-3sI-tS37IK_zUmvhOrzU8o5FI" /> <span className="ms-3">| &hearts; Toncoin</span>
         </div>
       </footer>
     </div>
