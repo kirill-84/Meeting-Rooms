6a7a959d73648a46d8d47baa186505d358aed09d
Kirill <35264591+kirill-84@users.noreply.github.com>
2026-01-27 14:47:28 +0300

    Update page.tsx

--- app/test-telegram/page.tsx
+++ app/test-telegram/page.tsx
@@ -22,7 +22,7 @@ export default function TestTelegram() {
     // Загружаем скрипт виджета
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.setAttribute("data-telegram-login", "ВАШ_BOT_USERNAME"); // замените!
+    script.setAttribute("data-telegram-login", "RoomCoordBot"); // замените!
     script.setAttribute("data-size", "large");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
     script.setAttribute("data-request-access", "write");
